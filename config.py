df5679ec2ba83a9fd5f1ee6421aec6bf4389e079
Ishan Patel <ishan@algoscale.com>
2014-10-09 18:03:11 +0530

    bugfix

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ VINE_API_BASE_URL = "https://api.vineapp.com/"
 
 config = ConfigParser.ConfigParser()
 config.read('config.txt')
-VIDEOS_LIMIT = config.get('system_section','size')
+VIDEOS_LIMIT = int(config.get('system_section','size'))
 ACCESS_TOKEN = "36207530.e2b196f.fc6b55f228d04f3bbb110629f4b1cc19"
